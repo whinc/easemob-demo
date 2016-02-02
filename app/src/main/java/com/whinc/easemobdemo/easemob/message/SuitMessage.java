080925c18eb7fa066e9849664c4544819c6b59db
whinc <xiaohui_hubei@163.com>
2016-02-02 20:01:32 +0800

    新增自定义消息 SuitMessage 及相关UI显示

--- app/src/main/java/com/whinc/easemobdemo/easemob/message/SuitMessage.java
+++ app/src/main/java/com/whinc/easemobdemo/easemob/message/SuitMessage.java
@@ -1,5 +1,7 @@
 package com.whinc.easemobdemo.easemob.message;
 
+import android.text.TextUtils;
+
 import com.easemob.chat.EMMessage;
 
 /**
@@ -9,28 +11,44 @@ import com.easemob.chat.EMMessage;
  * Email:xiaohui_hubei@163.com</p>
  */
 public class SuitMessage extends CommonMessage2{
-    public static final String GLAMOUR = "glamour";
+    public static final String COMFORT = "comfort";
     public static final String FLAUNT = "flaunt";
+    public static final String KEYWORDS = "keywords";
+    private float flaunt;       // 性价比
+    private float comfort;      // 舒适度（匹配度）
+    private String keywords;    // 关键字（空白字符分割）
 
-    public float getGlamour() {
-        return glamour;
+    public SuitMessage(EMMessage message) {
+        super(message);
     }
 
     public float getFlaunt() {
         return flaunt;
     }
 
-    private float glamour;      // 潮流值（魅力值）
-    private float flaunt;       // 性价比
+    public float getComfort() {
+        return comfort;
+    }
 
-    public SuitMessage(EMMessage message) {
-        super(message);
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public String[] getKeywordArray(){
+        String[] result;
+        if (!TextUtils.isEmpty(keywords)) {
+            result = keywords.trim().split("\\s+");
+        } else {
+            result = new String[0];
+        }
+        return result;
     }
 
     @Override
     protected void parse(EMMessage message) {
         super.parse(message);
-        glamour = Float.valueOf(message.getStringAttribute(GLAMOUR, "0"));
+        comfort = Float.valueOf(message.getStringAttribute(COMFORT, "0"));
         flaunt = Float.valueOf(message.getStringAttribute(FLAUNT, "0"));
+        keywords = message.getStringAttribute(KEYWORDS, "");
     }
 }
--- app/src/main/java/com/whinc/easemobdemo/easemob/widget/chatrow/SuitChatRow.java
+++ app/src/main/java/com/whinc/easemobdemo/easemob/widget/chatrow/SuitChatRow.java
@@ -1,7 +1,8 @@
 package com.whinc.easemobdemo.easemob.widget.chatrow;
 
 import android.content.Context;
-import android.graphics.Paint;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -10,6 +11,9 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.whinc.easemobdemo.R;
 import com.whinc.easemobdemo.easemob.message.SuitMessage;
+import com.zhy.view.flowlayout.FlowLayout;
+import com.zhy.view.flowlayout.TagAdapter;
+import com.zhy.view.flowlayout.TagFlowLayout;
 
 /**
  * 搭配消息
@@ -19,9 +23,9 @@ import com.whinc.easemobdemo.easemob.message.SuitMessage;
  */
 public class SuitChatRow extends BaseChatRow {
     private ImageView mThumbnailImg;
-    private TextView mIntroductionTxt;
-    private TextView mOnlinePriceTxt;
-    private TextView mOfflinePriceTxt;
+    private TextView mContentTxt;
+    private TagFlowLayout mKeywordFlowLayout;
+    private TextView mComfortTxt;
 
     private final SuitMessage mMessage;
 
@@ -38,10 +42,9 @@ public class SuitChatRow extends BaseChatRow {
     @Override
     protected void onFindViewById() {
         mThumbnailImg = (ImageView) findViewById(R.id.thumbnail_img);
-        mIntroductionTxt = (TextView) findViewById(R.id.content_txt);
-        mOnlinePriceTxt = (TextView) findViewById(R.id.glamour_label_txt);
-        mOfflinePriceTxt = (TextView) findViewById(R.id.offline_price_txt);
-        mOfflinePriceTxt.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);    // 文字添加删除线
+        mContentTxt = (TextView) findViewById(R.id.content_txt);
+        mKeywordFlowLayout = (TagFlowLayout) findViewById(R.id.keyword_flowLayout);
+        mComfortTxt = (TextView) findViewById(R.id.comfort_txt);
     }
 
     @Override
@@ -52,15 +55,31 @@ public class SuitChatRow extends BaseChatRow {
     @Override
     protected void onSetUpView() {
         Glide.with(getContext()).load(mMessage.getPicture()).into(mThumbnailImg);
-        mIntroductionTxt.setText(mMessage.getContent());
-        String onlinePrice = getResources().getString(R.string.params_online_price, mMessage.getOnline());
-        mOnlinePriceTxt.setText(onlinePrice);
-        String offlinePrice = getResources().getString(R.string.params_offline_price, mMessage.getOffline());
-        mOfflinePriceTxt.setText(offlinePrice);
+        mContentTxt.setText(mMessage.getContent());
+
+        String[] data = mMessage.getKeywordArray();
+        mKeywordFlowLayout.setAdapter(new CustomTagAdapter(data));
+        mKeywordFlowLayout.setVisibility(data.length == 0 ? View.GONE : View.VISIBLE);
     }
 
     @Override
     protected void onBubbleClick() {
         Toast.makeText(getContext(), "clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    static class CustomTagAdapter extends TagAdapter<String> {
+
+        public CustomTagAdapter(String[] datas) {
+            super(datas);
+        }
+
+        @Override
+        public View getView(FlowLayout parent, int pos, String keyword) {
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            View view = inflater.inflate(R.layout.include_keyword_label, parent, false);
+            TextView keywordTxt = (TextView) view.findViewById(R.id.text1);
+            keywordTxt.setText(keyword);
+            return view;
+        }
     }
 }
